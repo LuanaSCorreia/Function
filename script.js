6cf817a57eed3ebab3bb868534b465c850be8725
LuanaSCorreia <luanacorreia797@gmail.com>
2022-06-16 09:47:56 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,33 @@
+function algo(){
+console.log('Olá')
+}
+
+algo()
+
+
+function nome (meu){
+    console.log(`Meu nome é ${meu}`)
+}
+nome('Luana')
+
+
+function info(nome, idade, musica){
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e meu estilo musical é ${musica}`)
+}
+
+info('Luana', '22', 'eclético')
+
+
+function karynne(filme, musica){
+    console.log(`${filme} é o filme preferido da Karynne e ${musica} é a música preferida`)
+}
+
+karynne('Monstros SA','baby shark')
+
+
+function triplo(numero){
+    return numero*3
+}
+
+console.log(triplo(7)) 
+
